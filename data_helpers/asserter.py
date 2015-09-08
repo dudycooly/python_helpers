c6d9b88c83a1d86bb792d23b9c1472576b99ab8e
calvinj <jay.naveen@gmail.com>
2015-09-09 00:23:37 +0100

    JSON data helpers

--- data_helpers/asserter.py
+++ data_helpers/asserter.py
@@ -3,23 +3,6 @@ __author__ = 'jp'
 from nose.tools import (assert_equal, assert_items_equal,assert_dict_equal)
 from datadiff import diff
 
-def filter_list_of_dict_with_attr_value(list_of_dict, attribute, value):
-    """Return matches for item with matching attribute value"""
-    if list_of_dict:
-        return [datadict for datadict in list_of_dict
-                if attribute in datadict and datadict[attribute] == value]
-
-def filter_list_of_dict_with_attr(list_of_dict, attribute):
-    """Return matches for item with an attribute"""
-    if list_of_dict:
-        return [datadict for datadict in list_of_dict
-                if attribute in datadict]
-
-def sort_nested_list_in_place(list_of_lists):
-    for sub_list in list_of_lists:
-        sub_list.sort()
-    list_of_lists.sort()
-
 def check_items_equal(actual_value, expected_value, msg=""):
     """
     :param actual_value:
